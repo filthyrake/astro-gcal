1bfff20cb12104e22712d92938cfa1ceacf686b5
Damen Knight <damen@knightspeed.com>
2024-02-18 15:33:09 -0800

    Update updateTables.py
    this is now mostly fleshed out, just need to start testing

--- updateTables.py
+++ updateTables.py
@@ -32,29 +32,6 @@ def get_secret():
     google_oauth_client_id = secret_dict['google_oauth_client_id']
     google_oauth_secret_id = secret_dict['google_oauth_secret_id']
 
-def update_calendar_events():
-    # Get items from old and new tables
-    old_table_items = get_table_items('ap_events_old')
-    new_table_items = get_table_items('ap_events_new')
-
-    # Compare date and time ranges
-    old_dates = {(item['date'], item['time']) for item in old_table_items}
-    new_dates = {(item['date'], item['time']) for item in new_table_items}
-
-    if old_dates != new_dates:
-        # Delete old events from Google Calendar
-        for item in old_table_items:
-            delete_gcal_event(item['GCalID'])
-
-        # Add new events to Google Calendar and update new table with gcal event ids
-        for item in new_table_items:
-            gcal_event_id = add_gcal_event(item['date'], item['time'])
-            update_table_with_gcal_id('ap_events_new', item['GCalID'], gcal_event_id)
-
-        # Copy new table to old table and delete all items from new table
-        copy_table('ap_events_new', 'ap_events_old')
-        delete_table_items('ap_events_new')
-
 def get_table_items(table_name):
     # Get all items from the table
     table = dynamodb.Table(table_name)
@@ -98,5 +75,69 @@ def add_gcal_event(date, time):
     # Return the ID of the new event
     return event['id']
 
+def update_table_with_gcal_id(table_name, item_id, gcal_event_id):
+    # Get the table
+    table = dynamodb.Table(table_name)
+
+    # Update the item with the Google Calendar event ID
+    table.update_item(
+        Key={
+            'GCalID': item_id
+        },
+        UpdateExpression='SET GCalID = :val1',
+        ExpressionAttributeValues={
+            ':val1': gcal_event_id
+        }
+    )
+
+def copy_table(source_table_name, destination_table_name):
+    # Get the source and destination tables
+    source_table = dynamodb.Table(source_table_name)
+    destination_table = dynamodb.Table(destination_table_name)
+
+    # Scan the source table
+    response = source_table.scan()
+
+    # Write each item to the destination table
+    with destination_table.batch_writer() as batch:
+        for item in response['Items']:
+            batch.put_item(Item=item)
+
+def delete_table_items(table_name):
+    # Get the table
+    table = dynamodb.Table(table_name)
+
+    # Scan the table
+    response = table.scan()
+
+    # Delete each item
+    with table.batch_writer() as batch:
+        for item in response['Items']:
+            batch.delete_item(Key={'GCalID': item['GCalID']})
+
+def update_calendar_events():
+    # Get items from old and new tables
+    old_table_items = get_table_items('ap_events_old')
+    new_table_items = get_table_items('ap_events_new')
+
+    # Compare date and time ranges
+    old_dates = {(item['date'], item['time']) for item in old_table_items}
+    new_dates = {(item['date'], item['time']) for item in new_table_items}
+
+    if old_dates != new_dates:
+        # Delete old events from Google Calendar
+        for item in old_table_items:
+            delete_gcal_event(item['GCalID'])
+
+        # Add new events to Google Calendar and update new table with gcal event ids
+        for item in new_table_items:
+            gcal_event_id = add_gcal_event(item['date'], item['time'])
+            update_table_with_gcal_id('ap_events_new', item['GCalID'], gcal_event_id)
+
+        # Copy new table to old table and delete all items from new table
+        copy_table('ap_events_new', 'ap_events_old')
+        delete_table_items('ap_events_new')
+
 def lambda_handler(event, context):
   get_secret()
+  update_calendar_events()
