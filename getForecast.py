a6828078c9e1ca0c0aad273b0c9b652826f316a0
Damen Knight <damen@knightspeed.com>
2024-02-18 11:35:57 -0800

    Update getForecast.py
    some refactoring/cleanup
    
    making it actually look like a lambda function and able to run in lambda without errors (assuming you have created the relevant errors)
    
    theoretically with the right permissions it should connect/write to dynamoDB now but have not yet tested that.

--- getForecast.py
+++ getForecast.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import pytz
 import boto3
 from pytz import timezone
 from astral import LocationInfo
@@ -31,58 +30,8 @@ good_seeing_transparency_clouds_offsets=[]
 final_good_offsets=[]
 
 # Let's set our location for the astral library
-city = LocationInfo("<city>", "<state>", "America/Los_Angeles", LAT, LONG)
+city = LocationInfo("<your city>", "<your state>", "America/Los_Angeles", LAT, LONG)
 antioch = Location(city)
-
-
-start_time = datetime.fromisoformat(json_object["LocalStartTime"])
-
-def time_in_range(start, end, current):
-    """Returns whether current is in the range [start, end]"""
-    return start <= current <= end
-
-
-for offset in json_object['Astrospheric_Seeing']:
-  if offset['Value']['ActualValue'] >= 2:
-    good_seeing_offsets.append(offset['HourOffset'])
-
-for offset in good_seeing_offsets:
-  transparency = json_object['Astrospheric_Transparency'][offset]
-  if transparency['Value']['ActualValue'] <= 23:
-    good_seeing_transparency_offsets.append(transparency['HourOffset'])
-
-for offset in good_seeing_transparency_offsets:
-  cloudcover = json_object['RDPS_CloudCover'][offset]
-  if cloudcover['Value']['ActualValue'] <= 30:
-    good_seeing_transparency_clouds_offsets.append(cloudcover['HourOffset'])
-
-for offset in good_seeing_transparency_clouds_offsets:
-  offset_time = start_time + timedelta(hours=offset)
-  offset_date = offset_time.date()
-  night_time = sun(city.observer, date=offset_date, tzinfo=antioch.timezone)
-  dusk = night_time['dusk']
-  dawn_time = sun(city.observer, date=(offset_date + timedelta(days=1)), tzinfo=antioch.timezone)
-  dawn = dawn_time['dawn']  
-  if (time_in_range(dusk, dawn, time_zone.localize(offset_time))):
-    final_good_offsets.append(offset)
-
-# generate events from the final_good_offsets list
-# events should have start and end times
-# this will be the basis for our calendar event generation later
-# for now we just want something easy to store in the tables/database later
-events = []
-
-for k, g in groupby(enumerate(final_good_offsets), lambda ix: ix[0] - ix[1]):
-  temp_list = list(map(itemgetter(1), g))
-  if len(temp_list) > 2:
-    event_start = start_time + timedelta(hours=temp_list[0])
-    event_end = start_time + timedelta(hours=temp_list[-1])
-    event = {
-      'start': event_start,
-      'end': event_end
-    }
-    events.append(event)
-
 
 # note to self: move this stuff elsewhere later
 # connect to DynamoDB
@@ -91,6 +40,17 @@ dynamodb = boto3.resource('dynamodb')
 # get the tables
 table_new = dynamodb.Table('ap_events_new')
 
+start_time = datetime.fromisoformat(json_object["LocalStartTime"])
+# generate events from the final_good_offsets list
+# events should have start and end times
+# this will be the basis for our calendar event generation later
+# for now we just want something easy to store in the tables/database later
+events = []
+
+def time_in_range(start, end, current):
+    """Returns whether current is in the range [start, end]"""
+    return start <= current <= end
+
 # populate the ap_events_new table in the database with the events
 def populate_table(events):
   # Write events to the table
@@ -98,5 +58,46 @@ def populate_table(events):
     for event in events:
       batch.put_item(Item=event)
 
-# populate "new" table in database
-populate_table(events)
+def lambda_handler(event, context):
+  for offset in json_object['Astrospheric_Seeing']:
+    if offset['Value']['ActualValue'] >= 2:
+      good_seeing_offsets.append(offset['HourOffset'])
+
+  for offset in good_seeing_offsets:
+    transparency = json_object['Astrospheric_Transparency'][offset]
+    if transparency['Value']['ActualValue'] <= 23:
+      good_seeing_transparency_offsets.append(transparency['HourOffset'])
+
+  for offset in good_seeing_transparency_offsets:
+    cloudcover = json_object['RDPS_CloudCover'][offset]
+    if cloudcover['Value']['ActualValue'] <= 30:
+      good_seeing_transparency_clouds_offsets.append(cloudcover['HourOffset'])
+
+  for offset in good_seeing_transparency_clouds_offsets:
+    offset_time = start_time + timedelta(hours=offset)
+    offset_date = offset_time.date()
+    night_time = sun(city.observer, date=offset_date, tzinfo=antioch.timezone)
+    dusk = night_time['dusk']
+    dawn_time = sun(city.observer, date=(offset_date + timedelta(days=1)), tzinfo=antioch.timezone)
+    dawn = dawn_time['dawn']  
+    if (time_in_range(dusk, dawn, time_zone.localize(offset_time))):
+      final_good_offsets.append(offset)
+
+  for k, g in groupby(enumerate(final_good_offsets), lambda ix: ix[0] - ix[1]):
+    temp_list = list(map(itemgetter(1), g))
+    if len(temp_list) > 2:
+      event_start = start_time + timedelta(hours=temp_list[0])
+      event_end = start_time + timedelta(hours=temp_list[-1])
+      event = {
+        'start': event_start,
+        'end': event_end
+      }
+      events.append(event)
+
+  # populate "new" table in database
+  populate_table(events)
+
+
+
+
+
