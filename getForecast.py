06b899758fc514d81c618c65aaa0c7b4f0f7c332
Damen Knight <damen@knightspeed.com>
2024-02-21 07:16:37 -0800

    Update getForecast.py
    the great re-factoring and simplifying has begun
    
    sadly I also broke basically everything doing this.

--- getForecast.py
+++ getForecast.py
@@ -11,9 +11,12 @@ from astral.sun import sun
 from itertools import groupby
 from operator import itemgetter
 from datetime import datetime, timedelta
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from googleapiclient.errors import HttpError
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 API_URL="https://astrosphericpublicaccess.azurewebsites.net/api/GetForecastData_V1"
 LAT = os.environ.get('LAT')
@@ -21,7 +24,13 @@ LONG = os.environ.get('LONG')
 MY_CITY = os.environ.get('CITY')
 MY_STATE = os.environ.get('STATE')
 MY_TIMEZONE = os.environ.get('TIMEZONE')
+cal_id = os.environ.get('googleCalendarID')
 json_object = json.loads('{}') 
+
+credentials = service_account.Credentials.from_service_account_file(
+    'credentials.json',
+    scopes=['https://www.googleapis.com/auth/calendar']
+)
 
 time_zone = timezone(MY_TIMEZONE)
 
@@ -40,16 +49,15 @@ good_seeing_transparency_offsets=[]
 good_seeing_transparency_clouds_offsets=[]
 final_good_offsets=[]
 
-# Let's set our location for the astral library
+# lets set our location for the astral library
 city = LocationInfo(MY_CITY, MY_STATE, MY_TIMEZONE, LAT, LONG)
 final_city = Location(city)
 
-# note to self: move this stuff elsewhere later
 # connect to DynamoDB
 dynamodb = boto3.resource('dynamodb')
   
 # get the tables
-table_new = dynamodb.Table('ap_events_new')
+table_old = dynamodb.Table('ap_events_old')
 
 events = []
 
@@ -69,8 +77,23 @@ def get_secret():
 
     secret_response = client.get_secret_value(SecretId=secret_name)
     secret_string = secret_response['SecretString']
-    secret_dict = json.loads(secret_string)  # Assuming JSON format
+    secret_dict = json.loads(secret_string)  
     API_KEY = secret_dict['API_KEY']
+
+def get_google_oauth_credentials_from_secrets_manager():
+    secret_name = "googleOAuthCalendar"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    secret_response = client.get_secret_value(SecretId=secret_name)
+    secret_string = secret_response['SecretString']
+    secret_dict = json.loads(secret_string)  # Assuming JSON format
+    return secret_dict['google_oauth_client_id'], secret_dict['google_oauth_client_secret']
 
 def event_exists(events, event):
     for existing_event in events:
@@ -83,27 +106,110 @@ def time_in_range(start, end, current):
     return start <= current <= end
 
 def populate_table(events):
-  # Write items to the table
-  with table_new.batch_writer() as batch:
+  # write items to the table
+  with table_old.batch_writer() as batch:
       for event in events:
-          # Convert start and end times to strings and set gCalID to "none"
+          # convert start and end times to strings and set gCalID to "none"
           times = f"{event['start']} - {event['end']}"
           item = {
-              'id': event['id'],  # Use the unique ID as the key
+              'id': event['id'], 
               'times': times,
               'gCalID': 'none'
           }
           batch.put_item(Item=item)
+
+def update_calendar_events(service):
+    try:
+        table_items = get_all_items_from_table(table_old)
+
+        table_dates = {(item['times'].split(' - ')[0], item['times'].split(' - ')[1]) for item in table_items}
+        new_dates = {(item['times'].split(' - ')[0], item['times'].split(' - ')[1]) for item in events}
+
+        if not events:
+            for item in table_items:
+                delete_event_from_google_calendar(service, item['gCalID'])
+            return
+
+        if table_dates != new_dates:
+            for item in table_items:
+                delete_event_from_google_calendar(service, item['gCalID'])
+
+            items_with_gcal_id = []
+            for item in events:
+                start_time, end_time = item['times'].split(' - ')
+                gcal_event_id = add_event_to_google_calendar(service, start_time, end_time)
+                item['gCalID'] = gcal_event_id
+                items_with_gcal_id.append(item)
+
+            delete_all_items_from_table(table_old)
+
+            with table_old.batch_writer() as batch:
+                for item in items_with_gcal_id:
+                    logging.debug(f"Writing item to table: {item}")
+                    batch.put_item(Item=item)
+
+    except Exception as e:
+        logging.error(f"An error occurred while updating calendar events: {e}")
+
+def get_all_items_from_table(table_old):
+    response = table_old.scan()  
+    return response['Items']
+
+def delete_event_from_google_calendar(service, event_id):
+    try:
+        service.events().delete(calendarId=cal_id, eventId=event_id).execute()
+    except HttpError as e:
+        if "Resource has been deleted" in str(e):
+            logging.warning(f"Attempted to delete an event that has already been deleted: {event_id}")
+            # Remove the event from the DynamoDB table
+            table_old.delete_item(Key={'gCalID': event_id})
+        else:
+            raise
+
+def add_event_to_google_calendar(service, start_time, end_time):
+    try:
+        logging.debug(f"Adding event with start time {start_time} and end time {end_time}")
+
+        event = {
+            'summary': 'Good Astro Weather',
+            'start': {
+                'dateTime': datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S').isoformat(),
+                'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+                'dateTime': datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S').isoformat(),
+                'timeZone': 'America/Los_Angeles',
+            },
+        }
+
+        logging.debug(f"Event details: {event}")
+
+        event = service.events().insert(calendarId=cal_id, body=event).execute()
+
+        logging.debug(f"Response from Google Calendar API: {event}")
+
+        return event['id']
+    except Exception as e:
+        logging.error(f"An error occurred while adding the event: {e}")
+        return None
+
+def delete_all_items_from_table(table):
+    response = table.scan()
+
+    with table.batch_writer() as batch:
+        for item in response['Items']:
+            batch.delete_item(Key={'gCalID': item['gCalID']})
 
 def lambda_handler(event, context):
   logging.info('Lambda function started')
   get_forecast()
   start_time = datetime.fromisoformat(json_object["LocalStartTime"])
 
+  #we only consider the seeing "Good" if it's 2 or higher
   good_seeing_offsets = [offset['HourOffset'] for offset in json_object['Astrospheric_Seeing'] if offset['Value']['ActualValue'] >= 2]
-
+  #we only consider the transparency "Good" if it's 23 or lower
   good_seeing_transparency_offsets = [offset for offset in good_seeing_offsets if json_object['Astrospheric_Transparency'][offset]['Value']['ActualValue'] <= 23]
-
+  #we only consider the clouds "Good" if it's 30 or lower (representing 30% cloud cover)
   good_seeing_transparency_clouds_offsets = [offset for offset in good_seeing_transparency_offsets if json_object['RDPS_CloudCover'][offset]['Value']['ActualValue'] <= 30]
 
   final_good_offsets = []
@@ -120,21 +226,24 @@ def lambda_handler(event, context):
   for k, g in groupby(enumerate(final_good_offsets), lambda ix: ix[0] - ix[1]):
       temp_list = list(map(itemgetter(1), g))
       if len(temp_list) > 2:
-          event_start = start_time + timedelta(hours=temp_list[0])
-          event_end = start_time + timedelta(hours=temp_list[-1])
-          # Adjust event start and end times to be within the range of dawn and dusk
-          event_start = max(event_start, dusk)
-          event_end = min(event_end, dawn)
-          # Check if the event duration is at least 2 hours
+          event_start = (start_time + timedelta(hours=temp_list[0])).replace(tzinfo=time_zone)
+          event_end = (start_time + timedelta(hours=temp_list[-1])).replace(tzinfo=time_zone)
+          # adjust event start and end times to be within the range of dawn and dusk
+          event_start = max(event_start, dusk.replace(tzinfo=time_zone))
+          event_end = min(event_end, dawn.replace(tzinfo=time_zone))
+          # check if the event duration is at least 2 hours
           if event_end - event_start >= timedelta(hours=2):
               event = {
                   'id': str(uuid.uuid4()),  # Add a unique ID to each event
                   'start': event_start,
                   'end': event_end
               }
-              # Check if event already exists in the list
+              # check if event already exists in the list
               if not event_exists(events, event):
                   events.append(event)
   # populate "new" table in database
   populate_table(events)
+  google_oauth_client_id, google_oauth_secret_id = get_google_oauth_credentials_from_secrets_manager()
+  service = build('calendar', 'v3', credentials=credentials)
+  update_calendar_events(service)
   logging.info('Lambda function completed')
